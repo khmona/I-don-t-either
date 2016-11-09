6efdb2d0093ce0c2225d72f91b989211dac4bd1d
Granche <granit.demiraj@hotmail.com>
2016-11-09 09:42:27 +0100

    Kopplat ihop dummyGenerator.js

--- Classes/REST.class.js
+++ Classes/REST.class.js
@@ -1,85 +1,82 @@
 'use strict';
 var s = g.settings;
+var dummyGen = require("../dummyGen.js");
 
 module.exports = class REST {
   constructor(express) {
     this.settings = s.REST;
-    this.DB = new g.classes.DB(); // DB connection & models
+    this.DB = new g.classes.DB(); 
     this.app = express;
 
     this.router();
   }
 
-  // setup standard CRUD for route
   router() {
     var me = this;
     this.app.all(this.settings.route, function(req, res) {
-      var model = me.DB.getModel(req.params.model);
-      // do we have a 404?
-      if (!me[req.method] || !model) {
-        res.sendStatus(404);
-        res.end();
-        return;
+      if(req.params.model == "ny"){
+        dummyGen();
       }
+      else {
+        var model = me.DB.getModel(req.params.model);
+        if (!me[req.method] || !model || !model == "ny") {
+          res.sendStatus(404);
+          res.end();
+          return;
+        }
 
-      // combine any data sent in the request body with
-      // any data sent in the request URL
-      var params = req.body || {};
-      params.model = req.params.model;
-      if (req.params.modelID) {
-        params.modelID = req.params.modelID;
+        var params = req.body || {};  // VARFÖR TOMT OBJEKT!
+        params.model = req.params.model; 
+        if (req.params.modelID) {
+          params.modelID = req.params.modelID;
+        }
+
+        me[req.method](model, params, req, res);
       }
-
-      // and call the appropriate method
-      me[req.method](model, params, req, res);
     });
   }
 
-  // CREATE
+  
   POST(model, params, req, res) {
     var me = this,
-        toSave = new model(params); // new model instance with data
+        toSave = new model(params); 
       
-    // write data to DB
     toSave.save(function(err, result) {
       if (err) { me.error(err, res); return; }
-      res.json(result); // respond with result
+      res.json(result);
     });
   }
 
-  // READ
   GET(model, params, req, res) {
-    // pick a mongoose query function and parameters for it
+    
     var me = this,
         func = params.modelID ? 'findById' : 'find',
         q = params.modelID ? params.modelID : {};
 
-    // call the query function (find || findById)
     model[func](q, function(err, result) {
       if (err) { me.error(err, res); return; }
-      res.json(result); // respond with result
+      res.json(result); 
     });
   }
   
-  // UPDATE
+  
   PUT(model, params, req, res) {
     if (!params.modelID) { this.error({error: 'Missing ID!'}, res); return; }
 
     var me = this;
     model.findByIdAndUpdate(params.modelID, params, {new: true}, function (err, result) {
       if (err) { me.error(err, res); return; }
-      res.json(result); // respond with result
+      res.json(result);
     });
   }
 
-  // DELETE
   DELETE(model, params, req, res) {
     if (!params.modelID) { this.error({error: 'Missing ID!'}, res); return; }
 
     var me = this;
     model.findByIdAndRemove(params.modelID, function(err, result) {
       if (err) { me.error(err, res); return; }
-      res.json(true); // respond with result
+      res.json(true); 
     });
   }
 
--- models/spare.js
+++ models/spare.js
@@ -3,7 +3,7 @@ var mongoose= require('mongoose');
 var spareSchema = mongoose.Schema({
 
 	serial: {type: String, required: true},
-	fitsModel: {type: Array, required: true},
+	fitsModel: {type: String, required: true},
 	description: {type: String, required: true},
 	price: {type: Number, required: true},
 
