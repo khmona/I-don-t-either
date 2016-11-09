a4afcefc18caa836e2fe222c823bf90e3b5d63ea
sadia2016 <sadiaalisweden@gmail.com>
2016-11-09 09:39:43 +0100

    dummy generator

---
+++ dummyGen.js
@@ -0,0 +1,71 @@
+var sparePart = require("./models/spare.js")
+var Damage = require("./models/damage.js");
+var Employee = require("./models/employee.js")
+var Customer = require("./models/customer.js")
+var Bil = require("./models/car.js")
+
+module.exports = function() {
+  serialArr = "2223X-D46QP9-D56BPN 2556LL-R5K4-V5PF 286E-T9PS-RAG2 2FVL-HES2-R4YM 368L-9VGD-NNN2 3BH8-HWVW-25H2 3L2B-RGPV-DKAR 4S23-5873-VKLM 5DGB-Q4E7-G6CZ 5KMM-JDZB-P29M 5WSD-ABVS-HCSM 5Z5T-4S4J-HQ8W 659J-P57F-EHWY 6BTQ-M6HG-3S3W 6CWL-P4QM-X5HW 6HTE-JSQG-BR35 6LRJ-8EF4-S5YS 73ZD-J9KV-8P89 7AVF-VZ2L-GN9B 7DYD-6ETQ-57UY 7GA2-9K2J-WF4N 7RPT-CNL7-5SXS 82L5-BYQ4-LSZD 8EFB-P6ZT-X8PH 8EY3-TVV8-5QHD 8H32-HBQH-J3G6 92P8-Q9KD-3VR4 9BFX-XJUA-6DZD APFF-HWMY-JVUL AT2V-6MET-VEN6 BF8K-LJJH-UPCG BG4Q-HJCG-BXJ8 BLBB-RKZQ-FRAM BXXG-RTN2-RDCP CJJT-KGDW-SBD7 CMPA-FUUQ-DN7T CN48-JR9J-Q9F7 CSVC-PEMN-K2PV CV7K-Y3N6-WMDK EPSN-AX5M-G76W EPWU-V96F-SSNT ESMG-GAAN-HKAT ETGF-Y8JV-FUH6 EYN7-722R-JDJ9 F9QD-PV7F-PGJJ FY6B-BLQQ-SGE9 G7T6-5X58-7BGW GCCN-U234-GHPJ GESB-K25S-HU5T GG8Z-TMQD-9XZ6 GMJJ-542D-ZZNW GSSQ-8CE6-FUUN GY76-WBFS-7AGN HQ5R-NQBT-DFA7 J346-DV3R-HCZD J3XJ-WUXX-KAB6 J5CQ-TJZE-VGUX J88B-GUFB-YGZX J9ZY-J28P-3LXA JBDX-J55M-9YPG JMSQ-XGDF-FGAU K3QP-ZSEN-FE2G KJXH-QGFL-JPYT KWW5-F8S2-9NEM L8FH-SHPH-YAF2 LT37-5A38-AN5N N2EJ-JLEM-VLCU NVGB-PYT6-MMYF P2QU-FMYL-7HET PF78-KJED-33FP PGSQ-N5QJ-NT45 Q7TT-EN3F-YSMM QMZ2-BZDF-8CPK S6YT-P3GK-59JB SHVQ-64UM-83C6 SJMJ-348L-682Y U3LJ-4RQF-283E U9XP-8H7E-A5MQ UR3T-PEUV-X5S6 UUMV-8UJ5-XZ9V V9XZ-MFYW-2MYM VN28-9JZD-ZUTU VN7G-CKD3-DJ2W W5K9-KSBA-4BHJ WAMN-E2TY-TZLJ WGHM-YNA4-N4MP WS5C-3ALR-997V WT68-9AHH-AJX6 X8ZL-9WJ7-A3BQ XGZM-WUUB-Q6S7 XMKH-VTJM-M4HS XU6F-6E77-8GCP XXNS-BMJX-NHPC Y5WG-WVS4-2PJE YSHG-FKHH-G8J3 YZ5R-E9GN-YJQN Z88G-G8GR-ZBMS ZEPD-DS48-CL24 ZHVR-RZJ2-A8GD ZXH8-F27J-8LBY".split("-");
+  modelArr = ["Mercedes Benz A2","Toyota Carrila -00","VW GOLF -12","Mercedes Benz E100 -16","Alfa Romeo 75","Audi A4 -99","BMW 327 -15","Bugatti EB110 -14","Chevrolet Opala -11","Jaguar XK -09","Jeep Wrangler -02","Lamborghini Urraco 03","Lincoln Town Car -66","Opel Corsa -87","Peugeot 206 -00","Porsche 928 -08","Renault 5 -06","Volvo 140 -55","Toyota Prius -14","Nissan Micra -88"];
+  descriptionArr = "Front Right Outer door handle-Front Left Side Outer door handle-Rear Right Side Outer door handle-Rear Left Side Outer door handle-Front Right Side Inner door handle-Front Left Side Inner door handle-Rear Right Side Inner door handle-Rear Left Side Inner door handle-Back Door Outer Door Handle-Front Right Side Window motor-Front Left Side Window motor-Rear Right Side Window motor-Alternator bearing-Alternator bracket-Alternator fan-Rectifier-Regulator-Stater-Rotor-Polly-Diode & Plate Assembly-Rotor Assembly-Retainer-Housing-Carbon Brushes-Slip Rings-Coil-Voltage Regulator".split("-");
+  priceArr = [149,299,249,349,399,449,499,549,599,649,699,749,799,849,899,949,999,1099,1049,1149,1199,1249,1299,1349,1399,1499,1549,1699,1799,1899,1999,1549,1649,1749,1849,1949];
+  descArr = ["Regular Service","Regular Service","Regular Service","Regular Service","Broken headlight","Engine failure","Tire change","Collision damage","Collision damage","Broken windows","Filter change","Oil change","broken fuel-gauge","Needs new stereo","Needs windows tinted","Brake failure"," Needs wheel adjustment","Won't start","Wont stop","Needs painting on rear right-side"];
+  nameArr = "Rafaela Borrego-Josefa Wiggins-Marivel Downer-Abraham Inskeep-Shelia Moad-Consuela Christner-Erich Mattei-Theda Huffstetler-Sherrill Defalco-Cinderella Dempsey-Ulysses Moton-Nathalie Harker-Leda Womac-Jackqueline Mcneil-Kasandra Knisely-Brianne Cantrelle-Chadwick Carrasco-Bettie Thomasson-Almeda Hilchey-Jenelle Chausse-Inger Canizales-Cassondra Treese-Herminia Blunk-Weldon Avants-Rupert Gard-Garnett Como-Alease Delmonte-Lue Vrba-Nereida Shadwick-Xuan Nygren-Manie Meadors-Almeta Peel-Benito Toms-Sonja Dy-Terina Younger-Juliana Bodine-Joey Keifer-Irina Grimaldi-Glennie Zimmermann-Alejandrina Cessna-Letitia Chancy-Tony South-Kieth Morissette-Berry Rappaport-Teresa Admire-Penney Heldt-Dayle Zamor-Xavier Nembhard-Dexter Browner-Diana Weintraub".split("-")
+  numberArr = "(995) 605-1549-(981) 652-5696-(563) 146-7238-(948) 282-2787-(780) 892-7822-(789) 868-3333-(542) 853-3342-(474) 101-1910-(179) 823-6679-(100) 227-8412-(729) 722-0811-(368) 947-5456-(458) 315-9824-(682) 424-0051-(377) 974-0592-(548) 708-4299-(289) 915-5991-(769) 854-1567-(138) 743-2271-(371) 198-5115-(776) 763-1869-(513) 986-3565".split("-");
+  regArr = ["ABC 123","DEF 345","GHI 456","JKL 567","MNO 678","PQR 789"," STU 890","VXY 012"," XYZ 123","KPG 856","HAM 041","AKF 222","YUA 621","AYH 999","GRA 666","NIT 333","FLY 696","TIO 337","HAL 239","OLA 753"];
+  modelArr = ["Mercedes Benz A2","Toyota Carrila -00","VW GOLF -12","Mercedes Benz E100 -16","Alfa Romeo 75","Audi A4 -99","BMW 327 -15","Bugatti EB110 -14","Chevrolet Opala -11","Jaguar XK -09","Jeep Wrangler -02","Lamborghini Urraco 03","Lincoln Town Car -66","Opel Corsa -87","Peugeot 206 -00","Porsche 928 -08","Renault 5 -06","Volvo 140 -55","Toyota Prius -14","Nissan Micra -88"];
+  usedRegs = [];
+
+  var aCustomer = new Customer({
+    name: nameArr[Math.round(Math.random()*nameArr.length-1)],
+    number: numberArr[Math.round(Math.random()*numberArr.length-1)],
+  })
+
+  aCustomer.save(function(err){
+    if(err) {console.log(err)}
+    console.log("Customer Sparad!")
+  
+    var aPart = new sparePart({
+      serial: serialArr[Math.round(Math.random()*serialArr.length-1)],
+      fitsModel: modelArr[Math.round(Math.random()*modelArr.length-1)],
+      description: descriptionArr[Math.round(Math.random()*descriptionArr.length-1)],
+      price: priceArr[Math.round(Math.random()*priceArr.length-1)]
+    })
+
+    aPart.save(function(err){
+      if(err) {console.log(err)}
+      
+      var aDamage = new Damage({
+        description: descArr[Math.round(Math.random()*descArr.length-1)],
+        hasWorkedOn: aCustomer._id,
+        hasWorkedForMinutes: Math.round(Math.random()*60),
+        sparePartsUsed: aPart._id,
+      }) 
+      aDamage.save(function(err){
+        if(err) {console.log(err)}
+        
+        var enBil = new Bil({
+          model: modelArr[Math.round(Math.random()*modelArr.length-1)],
+          damage: aDamage._id,
+          customer: aCustomer._id,
+        });
+
+        var aReg = regArr[Math.round(Math.random()*regArr.length-1)];
+        
+        for(var i=0; i<usedRegs.length; i++) {
+          if(aReg == usedRegs[i]){
+            aReg = regArr[Math.round(Math.random()*regArr.length-1)];
+          }
+          continue    
+        }
+        enBil.registration = aReg;
+        usedRegs.push(aReg);
+
+        enBil.save(function(err){
+          if(err) {console.log(err)}
+        }); 
+      });
+    });
+  });
+};
+
